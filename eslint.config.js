8ab3877d20723a5d776f599380dc6d12d31cc583
Jordan Hood <jordan.hood@holidayextras.com>
2026-01-08 17:55:22 +0000

    Update dependencies and tooling
    Add Vitest, coverage, and UI packages
    Integrate Prettier into ESLint config
    Update test scripts to use Vitest
    Rename package to typespec-fastify

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,29 @@
 // @ts-check
 import eslint from "@eslint/js";
 import tsEslint from "typescript-eslint";
+import prettierConfig from "eslint-config-prettier";
+import prettierPlugin from "eslint-plugin-prettier";
 
 export default tsEslint.config(
   {
-    ignores: ["**/dist/**/*", "**/.temp/**/*"],
+    ignores: [
+      "**/dist/**/*",
+      "**/.temp/**/*",
+      "**/tsp-output/**/*",
+      "**/test/fixtures/tsp-output/**/*",
+      "**/coverage/**/*",
+      "**/examples/**/*",
+    ],
   },
   eslint.configs.recommended,
   ...tsEslint.configs.recommended,
+  prettierConfig,
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      "prettier/prettier": "error",
+    },
+  },
 );
